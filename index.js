495abb4704e73aae421c884f8747fd981b20cf0a
Tushar Sadhwani <18bit160.ietdavv.edu.in>
2020-05-03 03:20:28 -0700

    added better search querying

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = new Vue({
     searchText: "",
     searchQuery: "",
     chosenBranch: "",
+    resultCount: 0,
   },
   created: function () {
     console.log("created");
@@ -15,6 +16,7 @@ var app = new Vue({
       fetch("https://sheetsu.com/apis/v1.0su/6c312ee92a42")
         .then((res) => res.json())
         .then((results) => {
+          console.log(results);
           this.apiResults = results.map((el) => {
             data = Object.entries(el);
             return {
@@ -23,12 +25,7 @@ var app = new Vue({
               name: data[2][1],
               branch: data[3][1],
               experience: data[4][1],
-              picture:
-                "https://drive.google.com/uc?" +
-                data[5][1]
-                  .split("?")[1]
-                  .split("&")
-                  .find((el) => el.startsWith("id")),
+              picture: "https://drive.google.com/uc?id=" + el.URL,
             };
           });
         });
@@ -55,19 +52,50 @@ var app = new Vue({
       const filteredResults = new Set();
       branchResults.forEach((el) => {
         if (
+          el.name
+            .trim()
+            .toLowerCase()
+            .startsWith(this.searchQuery.toLowerCase())
+        ) {
+          filteredResults.add(el);
+        }
+      });
+      console.log(filteredResults);
+      if (filteredResults.size >= 10) {
+        this.resultCount = 10;
+        return Array.from(filteredResults).slice(0, 10);
+      }
+
+      branchResults.forEach((el) => {
+        if (
+          el.name
+            .trim()
+            .toLowerCase()
+            .split(" ")
+            .some((el) => el.startsWith(this.searchQuery.toLowerCase()))
+        ) {
+          filteredResults.add(el);
+        }
+      });
+      console.log(filteredResults);
+      if (filteredResults.size >= 10) {
+        this.resultCount = 10;
+        return Array.from(filteredResults).slice(0, 10);
+      }
+
+      branchResults.forEach((el) => {
+        if (
           el.name.trim().toLowerCase().includes(this.searchQuery.toLowerCase())
         ) {
           filteredResults.add(el);
         }
       });
       console.log(filteredResults);
-      if (filteredResults.size >= 10)
+      if (filteredResults.size >= 10) {
+        this.resultCount = 10;
         return Array.from(filteredResults).slice(0, 10);
-
-      if (filteredResults.size === 0) {
-        return [{ none: true }];
       }
-
+      this.resultCount = filteredResults.size;
       return filteredResults;
     },
   },
